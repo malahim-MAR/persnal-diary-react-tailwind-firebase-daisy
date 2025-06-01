5428e972815abedade5601764b31f08eec62203d
malahim-mar <armalahim01@gmail.com>
2025-06-02 01:48:26 +0500

    Firebase Setup Done

---
+++ src/lib/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAd37WHhsj3dT1CuJelb2T3E5s0aflr1uo",
+  authDomain: "persnal-diary.firebaseapp.com",
+  projectId: "persnal-diary",
+  storageBucket: "persnal-diary.firebasestorage.app",
+  messagingSenderId: "58200790788",
+  appId: "1:58200790788:web:79ed7409d441c92399e251"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
